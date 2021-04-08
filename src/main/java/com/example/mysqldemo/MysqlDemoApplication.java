4330143be744d82ad585498c7a43d6309e02b6d2
ibmjavanh01 <ibmjavanh01@iiht.tech>
2021-04-08 14:17:03 +0530

    UI integration

--- src/main/java/com/example/mysqldemo/MysqlDemoApplication.java
+++ src/main/java/com/example/mysqldemo/MysqlDemoApplication.java
@@ -2,12 +2,12 @@ package com.example.mysqldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+//@EnableTransactionManagement
 @SpringBootApplication
 public class MysqlDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MysqlDemoApplication.class, args);
 	}
-
 }
