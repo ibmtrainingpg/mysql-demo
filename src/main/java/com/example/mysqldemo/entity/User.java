7be902ce84523c6bdc27ab2b69c116138188c255
ibmjavanh01 <ibmjavanh01@iiht.tech>
2021-04-14 11:32:33 +0530

    added dat of birth

--- src/main/java/com/example/mysqldemo/entity/User.java
+++ src/main/java/com/example/mysqldemo/entity/User.java
@@ -1,5 +1,7 @@
 package com.example.mysqldemo.entity;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -14,24 +16,40 @@ public class User {
 	@NotNull
 	@NotBlank
 	private String name;
-//	
+	private Date dob;
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	//
 //	email;
 	private long phoneNumber;
+
 	public long getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
